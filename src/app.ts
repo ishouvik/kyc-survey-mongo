39b09b3f2d1e2bc23fcbafd92fdce5f0c85a12f7
Shouvik Mukherjee <contact@ishouvik.com>
2023-07-11 20:34:45 +0530

    feaet(src/app.ts): add news middlewares, routes

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 
+import { beforeAction, afterAction } from './middlewares/reqCallbacks'
+import * as dbConnection from './middlewares/dbConnection'
+
 import apiV1Welcome  from './routes/api/v1/welcome'
+import formResponses from './routes/api/v1/formResponses'
 
 /**
  * Routes
@@ -14,6 +18,13 @@ app.use(helmet())
 app.use(express.json())
 app.disable('x-powered-by')
 
+app.use(beforeAction)
+app.use(dbConnection.connect)
+
 app.use('*/api/v1/welcome', apiV1Welcome)
+app.use('*/api/v1/form-responses', formResponses)
+
+app.use(dbConnection.close)
+app.use(afterAction)
 
 export default app
